8a53d479438485c8e17b4dab3c9fb6fcbb403133
Dennis <blinhic850@mail.ru>
2020-11-03 15:22:24 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,12 +4,15 @@ from covid import Covid
 bot = telebot.TeleBot("1483059988:AAFQaZeheQXtBWSVnn4OY2hh6BoxfqBE3ek")
 print('Бот работает!')
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands = ['start'])
 def send_welcome(message):
 	bot.reply_to(message, "Привет! Пока что я тестовый бот и ничего не умею, но скоро во мне будет очень много строк кода, так что попрошу немного подождать :)\n"
-                          "Держи мою первую команду: /covid [country]")
-
-@bot.message_handler(func=lambda m: True)
+                          "Держи мои команды:\n1) /covid [country]\n2) /weather [город]")
+@bot.message_handler(commands = ['help'])
+def send_help(message):
+	bot.reply_to(message, "Привет! Вот все мои команды:\n1) /covid [country]\n2) /weather [город]")
+                          
+@bot.message_handler(func = lambda m: True)
 def echo_message(message):
     if str(message.text)[:6] == '/covid':
         country = str(message.text)[7:]
@@ -30,6 +33,6 @@ def echo_message(message):
                          f'Зафиксировано смертей: {deaths_country_cases}')
         bot.send_message(message.from_user.id, covid_message)
     else:
-        bot.send_message(message.from_user.id, "Я пока что не знаю, что мне на это ответить. Пожалуйста, пропиши команду /covid")
+        bot.send_message(message.from_user.id, "Я пока что не знаю, что мне на это ответить. Пожалуйста, пропиши команду /help")
 
 bot.polling(none_stop = True)
